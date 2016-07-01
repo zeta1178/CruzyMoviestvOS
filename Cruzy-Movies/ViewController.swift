0edc2c2c0a8e96c20d753e2aaf42ee3f67a30bc2
Michael Cruz <michaelcruz@michaels-mini>
2016-07-01 16:02:41 -0400

    Initial Commit

---
+++ Cruzy-Movies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cruzy-Movies
+//
+//  Created by Michael Cruz on 7/1/16.
+//  Copyright © 2016 Michael Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
