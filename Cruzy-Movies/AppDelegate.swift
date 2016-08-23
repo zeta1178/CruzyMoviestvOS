b02c216e70e0e689ad988a3f0cef17e02c81d7fc
Michael Cruz <michaelcruz@aim.com>
2016-08-22 22:48:27 -0400

    My First Commit

--- Cruzy-Movies/AppDelegate.swift
+++ Cruzy-Movies/AppDelegate.swift
@@ -2,45 +2,30 @@
 //  AppDelegate.swift
 //  Cruzy-Movies
 //
-//  Created by Michael Cruz on 7/1/16.
-//  Copyright © 2016 Michael Cruz. All rights reserved.
-//
+//  Created by Michael Cruz on 07/01/16.
+//  Copyright © 2016 Michael Cruz. All rights reserved.//
 
+
+import TVMLKit
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDelegate {
     var window: UIWindow?
-
-
+    var appController: TVApplicationController?
+    static let AWSURL = "https://s3.amazonaws.com/www.cruzy.co/images/Aladdin.jpg"
+    static let TVBaseURL = "https://s3.amazonaws.com/www.cruzy.co/s3files/"
+    static let TVBootURL = "\(AppDelegate.TVBaseURL)js/application.js"
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main().bounds)
+        let appControllerContext = TVApplicationControllerContext()
+        guard let javaScriptURL = NSURL(string: AppDelegate.TVBootURL) else { fatalError("unable to create NSURL") }
+        appControllerContext.javaScriptApplicationURL = javaScriptURL as URL
+        appControllerContext.launchOptions["BASEURL"] = AppDelegate.TVBaseURL
+        appControllerContext.launchOptions["AWSURL"] = AppDelegate.AWSURL
+        appController = TVApplicationController(context: appControllerContext, window: window, delegate: self)
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+    
 }
-
--- Cruzy-Movies/ViewController.swift
+++ Cruzy-Movies/ViewController.swift
@@ -2,24 +2,22 @@
 //  ViewController.swift
 //  Cruzy-Movies
 //
-//  Created by Michael Cruz on 7/1/16.
-//  Copyright © 2016 Michael Cruz. All rights reserved.
-//
+//  Created by Michael Cruz on 07/01/16.
+//  Copyright © 2016 Michael Cruz. All rights reserved.//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
