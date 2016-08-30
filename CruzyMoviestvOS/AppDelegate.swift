f101181484341cb2a67893700214cb049177f85f
Michael Cruz <michaelcruz@aim.com>
2016-08-30 13:58:04 -0400

    connecting all new files in one commit

---
+++ CruzyMoviestvOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Cruzy-Movies
+//
+//  Created by Michael Cruz on 07/01/16.
+//  Copyright © 2016 Michael Cruz. All rights reserved.//
+
+
+import TVMLKit
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDelegate {
+    var window: UIWindow?
+    var appController: TVApplicationController?
+    static let TVBaseURL = "http://cruzy.co/"
+    static let TVBootURL = "\(AppDelegate.TVBaseURL)js/application.js"
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        let appControllerContext = TVApplicationControllerContext()
+        guard let javaScriptURL = NSURL(string: AppDelegate.TVBootURL) else { fatalError("unable to create NSURL") }
+        appControllerContext.javaScriptApplicationURL = javaScriptURL
+        appControllerContext.launchOptions["BASEURL"] = AppDelegate.TVBaseURL
+        appController = TVApplicationController(context: appControllerContext, window: window, delegate: self)
+        return true
+    }
+    
+}
