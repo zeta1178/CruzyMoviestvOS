a485c3af71fa30ae2ee22bfd281e1c80678ec695
Michael Cruz <michaelcruz@aim.com>
2016-08-30 13:16:44 -0400

    updated the cruzy blue icon , moved over to root of cruzy.co, inserted variable BASEURL to the js file, updated for 9.3 TVOS

--- Cruzy-Movies/AppDelegate.swift
+++ Cruzy-Movies/AppDelegate.swift
@@ -13,17 +13,15 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDelegate {
     var window: UIWindow?
     var appController: TVApplicationController?
-    static let AWSURL = "https://s3.amazonaws.com/www.cruzy.co/images/Aladdin.jpg"
-    static let TVBaseURL = "https://s3.amazonaws.com/www.cruzy.co/s3files/"
+    static let TVBaseURL = "http://cruzy.co/"
     static let TVBootURL = "\(AppDelegate.TVBaseURL)js/application.js"
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main().bounds)
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
         let appControllerContext = TVApplicationControllerContext()
         guard let javaScriptURL = NSURL(string: AppDelegate.TVBootURL) else { fatalError("unable to create NSURL") }
-        appControllerContext.javaScriptApplicationURL = javaScriptURL as URL
+        appControllerContext.javaScriptApplicationURL = javaScriptURL
         appControllerContext.launchOptions["BASEURL"] = AppDelegate.TVBaseURL
-        appControllerContext.launchOptions["AWSURL"] = AppDelegate.AWSURL
         appController = TVApplicationController(context: appControllerContext, window: window, delegate: self)
         return true
     }
